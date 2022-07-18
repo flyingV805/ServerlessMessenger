005fd64057260e9faa7c115468483dad9d4ef7e7
V <petrov.vladislav1992@gmail.com>
2022-07-18 17:19:08 +0600

    Update SetupScreen logic

---
+++ app/src/main/java/kz/flyingv/serverlessmessenger/ui/screen/setup/SetupCompleteFragment.kt
@@ -0,0 +1,47 @@
+package kz.flyingv.serverlessmessenger.ui.screen.setup
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+@Composable
+fun SetupCompleteFragment(navController: NavController) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.Asset("chating.json"))
+    Column(
+        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LottieAnimation(
+            composition,
+            modifier = Modifier.fillMaxWidth(0.6f).wrapContentHeight(),
+            restartOnPlay = true,
+            iterations = 3
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("Welcome to ServerlessMessenger", textAlign = TextAlign.Center)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("Fast, Easy, Secure\nNo servers, No data collecting", textAlign = TextAlign.Center)
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            modifier = Modifier
+                .height(56.dp)
+                .fillMaxWidth(0.6f),
+            onClick = {
+
+            }
+        ) {
+            Text("Authorize")
+        }
+    }
+}
